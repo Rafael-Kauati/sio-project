4b6eaff2a4cc47e720b7435cb5ff72b783aa29c3
tk <kauati.toiti@gmail.com>
2024-11-07 15:22:43 +0000

    loggin support for routes

--- delivery1/repo-app/api/__init__.py
+++ delivery1/repo-app/api/__init__.py
@@ -1,3 +1,5 @@
+import logging
+
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
@@ -16,20 +18,32 @@ app.config['UPLOAD_FOLDER'] = os.path.join(os.path.dirname(__file__), 'uploads')
 if not os.path.exists(app.config['UPLOAD_FOLDER']):
     os.makedirs(app.config['UPLOAD_FOLDER'])
 
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    handlers=[
+        logging.StreamHandler(),  # Print to terminal
+        logging.FileHandler("repository.log")  # Write to log file
+    ]
+)
+
+logger = logging.getLogger(__name__)
+
+# Test log entry
+logger.info("Logging configuration test.")
 
 # Import your models
 from .models import Document, Organization, Session, Subject
 
 # Create the tables in the database
 with app.app_context():
-    db.drop_all()
-    db.create_all()  # This will create all tables defined in your models
+    #db.drop_all()
+    db.create_all()
 
     # Verify if the organization table exists
     result = db.session.execute("SELECT * FROM information_schema.tables WHERE table_name = 'organization'")
     for row in result:
         print(row)
 
-# Import your routes
 from .routes import main_bp
 app.register_blueprint(main_bp)
--- delivery1/repo-app/api/routes.py
+++ delivery1/repo-app/api/routes.py
@@ -1,81 +1,105 @@
+import logging
 from flask import Blueprint, jsonify, request, current_app, send_from_directory
 from api.controllers import OrganizationController, SessionController, DocumentController
-from werkzeug.utils import secure_filename
-import os
+from api import logger
 
 ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
+
 
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
+
 main_bp = Blueprint('main', __name__)
+
 
 #######################################################################
 #################### Anonymous API ####################################
 #######################################################################
 @main_bp.route('/organizations', methods=['POST'])
 def create_organization_route():
-    return OrganizationController.create_organization()
+    logger.info("Request to create organization received.")
+    response = OrganizationController.create_organization()
+    logger.info("Organization created successfully.")
+    return response
+
 
 @main_bp.route('/organizations', methods=['GET'])
 def list_organizations_route():
-    return OrganizationController.list_organizations()
+    logger.info("Request to list organizations received.")
+    response = OrganizationController.list_organizations()
+    logger.info("Organizations listed successfully.")
+    return response
 
-# Endpoint de criação de sessão
+
+# Endpoint for creating a session
 @main_bp.route('/sessions', methods=['POST'])
 def create_session_route():
-    return SessionController.create_session()
+    logger.info("Request to create session received.")
+    response = SessionController.create_session()
+    logger.info("Session created successfully.")
+    return response
 
-# Endpoint para download de arquivo
+
+# Endpoint for file download
 @main_bp.route('/download/<filename>', methods=['GET'])
 def download_file(filename):
     try:
+        logger.info(f"Request to download file: {filename}")
         return send_from_directory(current_app.config['UPLOAD_FOLDER'], filename, as_attachment=True)
     except FileNotFoundError:
+        logger.error(f"File not found: {filename}")
         return jsonify({'error': 'File not found'}), 404
+
 
 #######################################################################
 #################### Authenticated API ################################
 #######################################################################
 @main_bp.route('/sessions/assume_role', methods=['POST'])
 def assume_role_route():
+    logger.info("Request to assume role received.")
     return SessionController.assume_role()
+
 
 @main_bp.route('/sessions/release_role', methods=['POST'])
 def release_role_route():
+    logger.info("Request to release role received.")
     return SessionController.release_role()
 
-# Endpoint para listar as roles da sessão
+
 @main_bp.route('/sessions/<string:session_key>/roles', methods=['GET'])
 def list_session_roles_route(session_key):
+    logger.info(f"Request to list roles for session: {session_key}")
     return SessionController.list_session_roles(session_key)
+
 
 @main_bp.route('/sessions/<string:session_key>/subjects', methods=['GET'])
 def get_subjects_by_session_key_route(session_key):
+    logger.info(f"Request to get subjects by session key: {session_key}")
     return SessionController.get_subjects_by_session_key(session_key)
+
 
 @main_bp.route('/organization/<string:session_key>/org/roles', methods=['GET'])
 def get_roles_by_session_key_route(session_key):
+    logger.info(f"Request to get roles by session key: {session_key}")
     return SessionController.get_roles_by_session_key(session_key)
+
 
 @main_bp.route('/sessions/<string:session_key>/documents', methods=['GET'])
 def get_documents_by_session_key_route(session_key):
-    # Obtém parâmetros da consulta
     username = request.args.get('username')
     date_str = request.args.get('date')
-    filter_type = request.args.get('filter_type', 'all')  # 'all', 'more_recent', 'older', etc.
+    filter_type = request.args.get('filter_type', 'all')
 
-    # Chama o controlador para obter documentos
+    logger.info(
+        f"Request to get documents by session key: {session_key} with filters - username: {username}, date: {date_str}, filter_type: {filter_type}")
     documents = DocumentController.get_documents_by_session_key(session_key, username, date_str, filter_type)
     return jsonify(documents), 200
-
-
 
 
 #######################################################################
 #################### Authorized API ###################################
 #######################################################################
-
 @main_bp.route('/add_subject', methods=['POST'])
 def add_subject_route():
     data = request.json
@@ -86,10 +110,14 @@ def add_subject_route():
     public_key = data.get("public_key")
 
     if not all([session_key, username, name, email, public_key]):
-        return jsonify({"error": "Todos os campos são obrigatórios: session_key, username, name, email, public_key"}), 400
+        logger.warning("Missing required fields for adding subject.")
+        return jsonify(
+            {"error": "Todos os campos são obrigatórios: session_key, username, name, email, public_key"}), 400
 
+    logger.info(f"Adding subject to organization with session key: {session_key}")
     result = SessionController.add_subject_to_organization(session_key, username, name, email, public_key)
     return jsonify(result), 201 if "id" in result else 400
+
 
 @main_bp.route('/add_document', methods=['POST'])
 def add_document_route():
@@ -98,37 +126,40 @@ def add_document_route():
     file = request.files.get("file")
 
     if not all([session_key, document_name, file]):
+        logger.warning("Missing required fields for adding document.")
         return jsonify({"error": "Todos os campos são obrigatórios: session_key, document_name, e o arquivo"}), 400
 
-    # Chama o controller para salvar o documento
+    logger.info(f"Adding document to organization with session key: {session_key} and document name: {document_name}")
     result = SessionController.upload_document_to_organization(session_key, document_name, file)
     return jsonify(result), 201 if "id" in result else 400
+
 
 @main_bp.route('/document/metadata', methods=['GET'])
 def get_document_metadata_route():
     session_key = request.headers.get('session_key')
     document_name = request.args.get('document_name')
-    
+
     if not document_name:
+        logger.warning("Document name parameter missing for metadata request.")
         return jsonify({"error": "O parâmetro 'document_name' é obrigatório"}), 400
 
-    # Chama a função do controlador para buscar os metadados e o conteúdo do documento
+    logger.info(f"Requesting document metadata for session key: {session_key} and document name: {document_name}")
     result = SessionController.get_document_metadata(session_key, document_name)
-
     return jsonify(result)
 
 
 @main_bp.route('/download_document/<session_key>/<document_name>', methods=['GET'])
 def download_document_route(session_key, document_name):
+    logger.info(f"Request to download document: {document_name} in session: {session_key}")
     result = SessionController.download_document(session_key, document_name)
     if result is None:
-        return jsonify({"error": "Document not found in organization"}), 404  # Documento não encontrado ou erro na sessão
+        logger.error(f"Document {document_name} not found in session {session_key}")
+        return jsonify({"error": "Document not found in organization"}), 404
     return result
+
 
 @main_bp.route('/delete_document/<session_key>/<string:document_name>', methods=['DELETE'])
 def delete_document_route(session_key, document_name):
-    if not session_key:
-        return jsonify({"error": "Session key is required"}), 400
-
+    logger.info(f"Request to delete document: {document_name} in session: {session_key}")
     result = SessionController.delete_document_from_organization(session_key, document_name)
     return jsonify(result), 200 if result['success'] else 400
