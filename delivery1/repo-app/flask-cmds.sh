27787ae83f98061aeb11db521364e7bfff74b490
tk <kauati.toiti@gmail.com>
2024-11-12 11:48:52 +0000

    command files update, usefull to run the app at once

--- delivery1/repo-app/flask-cmds.sh
+++ delivery1/repo-app/flask-cmds.sh
@@ -4,8 +4,8 @@ export FLASK_ENV=development
 flask run
 
 #In case needs to migrate changes on the DB 
-flask db init
-flask db migrate -m "migrate changes description"
-flask db upgrade
+#flask db init
+#flask db migrate -m "migrate changes description"
+#flask db upgrade
 
 
