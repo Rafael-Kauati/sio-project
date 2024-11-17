c5209a2ca672f4d98aeaba67ff85ae9774924b68
tk <kauati.toiti@gmail.com>
2024-11-17 15:06:11 +0000

    flask commands update

--- delivery1/repo-app/flask-cmds.sh
+++ delivery1/repo-app/flask-cmds.sh
@@ -1,6 +1,8 @@
 #Setup the env for development
 export FLASK_APP=api  
 export FLASK_ENV=development
+export REP_ADDRESS="localhost:5000"
+export REP_PUB_KEY="public_key.pem"
 flask run
 
 #In case needs to migrate changes on the DB 
